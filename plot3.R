2a033ff746cb5b5ec857c7772e236dbb13990f40
vaddss <pupuzel1980@mail.ru>
2014-07-11 22:08:05 +0400

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,22 @@
+# Step 1 (getting the dataset):
+temp <- tempfile()
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, temp)
+dataset <- read.table(unz(temp,"household_power_consumption.txt"), header=T, na.strings = "?", sep=";")
+
+# Step 2 (subsetting our dataset):
+data <- subset(dataset, Date == "1/2/2007" | Date == "2/2/2007" )
+
+# Step 3 (converting date and time):
+data$DateTime <- paste(data$Date, data$Time)
+data$DateTime <- strptime(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
+
+# Step 4 (creating a plot):
+with(data,{ 
+     plot(DateTime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+     lines(DateTime, Sub_metering_2, col = "red")
+     lines(DateTime, Sub_metering_3, col = "blue")
+})
+legend("topright", lty = 1, lwd = 2, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3")) 
+dev.copy(png, "plot3.png", height=480, width=480)
+dev.off()
